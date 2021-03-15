a94dafedfa7ef00863ec97f82e19c759173e9913
luca <luca.lorenzetti0@gmail.com>
2021-03-15 15:51:44 +0100

    Inserita funzionalità menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(function(){
-    
+    $('.link_menu').click(function(){
+        $(this).next().toggleClass("active");
+    });
+
+    $('.link_menu').mouseenter(function () { 
+        $('.active').toggleClass("active");
+        $(this).next().toggleClass("active");
+    });
 });
